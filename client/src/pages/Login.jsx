f094010c5371c630eae52b9635779e3b7a2cdabd
Himanshukabsuri <hkabsuri@gmail.com>
2026-05-08 12:46:37 +0530

    login work properly

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,18 +1,43 @@
+import axios from 'axios';
 import React from 'react'
-
+import { useState } from 'react';
 const Login = () => {
+    const [formData,setFormData] = useState({
+        email:"",
+        password:""
+    })
+
+    const handleChange = (e)=>{
+        setFormData({
+            ...formData,[e.target.name]:e.target.value
+        })
+    }
+
+    const handleSubmit = async(e)=>{
+        e.preventDefault()
+
+        try {
+            const res = await axios.post("http://localhost:5000/api/auth/login",formData)
+
+            console.log(res.data);
+
+            alert("Login successfully")
+        } catch (error) {
+            console.log(error);
+            alert("Something went wrong");
+        }
+    }
   return (
     <div className='min-h-screen flex items-center justify-center bg-gray-100'>
-      <form className='bg-white p-8 rounded-xl shadow-md w-[400px]'>
-        <h1 className='text-3xl font-bold mb-6 text-center'>Login</h1>
-        <input className='w-full border p-3 rounded mb-4' type="text" name="" id="" />
-        <input className='w-full border p-3 rounded mb-4' type="text" name="" id="" />
-        <input className='w-full border p-3 rounded mb-4' type="text" name="" id="" />
+            <form onSubmit={handleSubmit} className='bg-white p-8 rounded-xl shadow-md w-[400px]'>
+                <h1 className='text-3xl font-bold mb-6 text-center'>Login</h1>
+                <input className='w-full border p-3 rounded mb-4' type="email" name="email" placeholder='email' onChange={handleChange} />
+                <input className='w-full border p-3 rounded mb-4' type="password" name="password" placeholder='password' onChange={handleChange} />
 
-        <button>Login</button>
-      </form>
-    </div>
-  )
-}
+                <button className='w-full bg-black text-white p-3 rounded' type='submit'>Login</button>
+            </form>
+        </div>
+  );
+};
 
-export default Login
+export default Login;
