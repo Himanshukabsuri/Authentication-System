0cf247bad76780ac86ca565dc95d6289af304d49
Himanshukabsuri <hkabsuri@gmail.com>
2026-05-08 12:07:51 +0530

    add frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Login/>} />
+        <Route path='/signup' element={<Signup/>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='min-h-screen flex items-center justify-center bg-gray-100'>
+      <form className='bg-white p-8 rounded-xl shadow-md w-[400px]'>
+        <h1 className='text-3xl font-bold mb-6 text-center'>Login</h1>
+        <input className='w-full border p-3 rounded mb-4' type="text" name="" id="" />
+        <input className='w-full border p-3 rounded mb-4' type="text" name="" id="" />
+        <input className='w-full border p-3 rounded mb-4' type="text" name="" id="" />
+
+        <button>Login</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login
